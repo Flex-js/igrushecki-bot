3c41ac80942c41a6ecaaf2628f5bf0a2912e17fd
Герасимов Алексей <gerasimov@tochkak.ru>
2019-09-18 00:26:25 +0300

    first iteration

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,1 +1,6 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const PORT = process.env.PORT || 3000;
+export const BOT_TOKEN = process.env.BOT_TOKEN || '';
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import express from 'express';
-
-import {PORT} from './config/app.config';
-
+import {PORT, BOT_TOKEN} from './config/app.config';
 import initMiddlewares from './middlewares/app.middlewares.js';
+import Telegraf from 'telegraf';
+import SocksAgent from 'socks5-https-client/lib/Agent';
+
+const socksAgent = new SocksAgent({
+	socksHost: '173.44.34.106',
+	socksPort: '34276'
+});
 
 const app = express();
-
 initMiddlewares(app);
+
+const bot = new Telegraf(BOT_TOKEN, {
+	telegram: {
+		agent: socksAgent
+	}
+});
+
+bot.start(ctx => ctx.reply('Welcome'));
+bot.catch(err => console.log('Ooops', err));
+bot.help(ctx => ctx.reply('Send me a sticker'));
+bot.on('sticker', ctx => ctx.reply('👍'));
+bot.hears('hi', ctx => ctx.reply('Hey there'));
+bot.launch();
 
 app.listen(PORT, '0.0.0.0', error => {
 	if (error) console.error(`Something going wrong, ${error}`);
