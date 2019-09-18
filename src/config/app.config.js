5570491dbb718a65b55276ae02ba478f0bde147c
Герасимов Алексей <gerasimov@tochkak.ru>
2019-09-18 18:40:54 +0300

    bot 2.2

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -2,10 +2,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const SECURE_TOKEN = process.env.SECURE_TOKEN || '';
 
 module.exports = {
-	PORT, BOT_TOKEN, SECURE_TOKEN
+	BOT_TOKEN, SECURE_TOKEN
 };
