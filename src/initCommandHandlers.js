c2e90c2bdf9da89f4eaacec847345b06f55aeebf
Суздалев Станислав <suzdalev@tochkak.ru>
2019-09-23 12:03:00 +0300

    Washing update fixes

--- src/initCommandHandlers.js
+++ src/initCommandHandlers.js
@@ -4,14 +4,17 @@ const initCommandHandlers = (bot) => {
 	bot.command('takoy_or', ctx => ctx.reply('😁'));
 	bot.command('zhest', ctx => ctx.replyWithSticker(STICKER_ZHEST));
 	bot.command('takoy_clown', ctx => ctx.replyWithSticker(STICKER_CLOWN));
-	bot.command('wash', ctx => {
+	bot.command('wash', async ctx => {
 		const reply = ctx.update.message.reply_to_message;
 		
 		if (reply) {
-			ctx.reply('💦💦💦', {reply_to_message_id: reply.message_id});
+			await ctx.reply('💦💦💦', {reply_to_message_id: reply.message_id});
 			ctx.reply('ПШШШШ ПШШШШ');
 		} else {
-			ctx.reply('_Используйте эту команду в ответ на сообщение, чтобы умыть собеседника_', {parse_mode: 'Markdown'});
+			ctx.reply(
+				'_Используйте эту команду в ответ на сообщение, чтобы умыть собеседника_',
+				{parse_mode: 'Markdown', reply_to_message_id: ctx.message.message_id}
+			);
 		}
 	});
 };
--- src/initTextHandlers.js
+++ src/initTextHandlers.js
@@ -4,9 +4,9 @@ const initTextHandlers = (bot) => {
 	bot.on('text', ctx => {
 		const {text} = ctx.message;
 		
-		if (text.includes('клоун')) {
+		if (text.toLowerCase().includes('клоун')) {
 			ctx.replyWithSticker(STICKER_CLOWN);
-		} else if (text.includes('?') && Math.random() > 0.5) {
+		} else if (text.includes('?')) {
 			ctx.replyWithSticker(STICKER_WASHING, {reply_to_message_id: ctx.message.message_id});
 		}
 	});
